94f06f2eeb83cefbbed86e1408da4086f4d9b033
JemmieLim <jemmielim88@gmail.com>
2023-05-18 07:39:43 +0700

    initial commit

--- src/server.js
+++ src/server.js
@@ -3,11 +3,16 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
+const users = require('./api/users');
 const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
+const ClientError = require('./exceptions/ClientError');
 
 const init = async () => {
   const notesService = new NotesService();
+  const usersService = new UsersService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -19,12 +24,50 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator,
-    },
+  await server.register(
+    [
+      {
+        plugin: notes,
+        options: {
+          service: notesService,
+          validator: NotesValidator,
+        },
+      },
+      {
+        plugin: users,
+        options: {
+          service: usersService,
+          validator: UsersValidator,
+        },
+      },
+    ],
+  );
+
+  server.ext('onPreResponse', (request, h) => {
+    const { response } = request;
+    if (response instanceof Error) {
+      if (response instanceof ClientError) {
+        const newResponse = h.response({
+          status: 'fail',
+          message: response.message,
+        });
+        newResponse.code(response.statusCode);
+        return newResponse;
+      }
+
+      if (!response.isServer) {
+        return h.continue;
+      }
+
+      const newResponse = h.response({
+        status: 'fail',
+        message: 'Maaf, terjadi kegagalan pada server kami',
+      });
+      newResponse.code(500);
+      console.error(response.message);
+      return newResponse;
+    }
+    return h.continue;
   });
 
   await server.start();
